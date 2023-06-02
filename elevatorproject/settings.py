6ed34a16e7411cd4ce12139fede27fdfd436d13d
Sonu <mishrasonu9211@gmail.com>
2023-06-03 00:31:15 +0530

    py.exe manage.py collectstatic

--- elevatorproject/settings.py
+++ elevatorproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--1=6z)p*xs9hwxl$#hc-%p%dz+dld_*p70(eqxbkkv5y8%g@+z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','pysk.pythonanywhere.com']
 
 
 # Application definition
@@ -133,6 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
